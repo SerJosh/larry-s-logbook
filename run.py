f66e312bf0f8bd9e27619a356f5600d7763acadb
SerJosh <>
2023-10-20 16:39:36 +0000

    add color tags to use in project from colorama, also comment in code notes for identification on what tags do

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 from beautifultable import BeautifulTable
 from colorama import Fore, Back, Style
+# Import textwrap to wrap long text for a better visual
 import textwrap
+
+# COLOR TAGS
+
+reset_all = Style.RESET_ALL           # Reset to normal
+d_color = Fore.LIGHTYELLOW_EX         # Data color
+q_color = Style.BRIGHT + Fore.GREEN   # Question color
+h_color = Style.BRIGHT + Fore.BLUE    # Header and Image color
